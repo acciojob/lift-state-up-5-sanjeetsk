969250bb1bba3cf309886bba19e50b9b3633869f
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-09 18:49:39 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <div className="parent">
+        <h1>Parent Component</h1>
+        {isLoggedIn ? (<p>You are logged in!</p>) : <Child setIsLoggedIn={setIsLoggedIn}/>}
+      </div>        
+    </div>
+  )
+}
+
+const Child = ({setIsLoggedIn}) =>{
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setIsLoggedIn(true);
+  }
+
+  return (
+    <div className="child">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Username: </label>
+        <input type="text" id="name" required />
+        <br/>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" required />
+        <br/>
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
